df00480824fb7f2ac7b7a4a024ea06778e6a4802
Martin Ohmann <martin.ohmann@lesara.de>
2018-11-01 23:51:51 +0100

    [native_gpio] add first native transmitter impl

--- internal/gpio/transmitter.go
+++ internal/gpio/transmitter.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
+
+	rpio "github.com/stianeikeland/go-rpio"
 )
 
 var logger *log.Logger
@@ -40,4 +43,63 @@ func (t *CodesendTransmitter) Transmit(code int, pulseLength int) error {
 	}
 
 	return exec.Command("codesend", args...).Run()
+}
+
+type NativeTransmitter struct {
+	gpioPin int
+}
+
+func NewNativeTransmitter(gpioPin int) *NativeTransmitter {
+	return &NativeTransmitter{
+		gpioPin: gpioPin,
+	}
+}
+
+// Transmit transmits the given code via the configured gpio pin
+func (t *NativeTransmitter) Transmit(code int, pulseLength int) error {
+	logger.Printf("transmitting code=%d pulseLength=%d\n", code, pulseLength)
+
+	err := rpio.Open()
+	if err != nil {
+		return err
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(t.gpioPin)
+	pin.Output()
+	binCode := fmt.Sprintf("%024b", code)
+
+	for i := 0; i < 10; i++ {
+		for _, c := range binCode {
+			switch c {
+			case '0':
+				t.send0(pin, pulseLength)
+			case '1':
+				t.send1(pin, pulseLength)
+			}
+		}
+		t.sendSync(pin, pulseLength)
+	}
+
+	return nil
+}
+
+func (t *NativeTransmitter) send0(pin rpio.Pin, pulseLength int) {
+	t.transmit(pin, 1, 3, pulseLength)
+}
+
+func (t *NativeTransmitter) send1(pin rpio.Pin, pulseLength int) {
+	t.transmit(pin, 3, 1, pulseLength)
+}
+
+func (t *NativeTransmitter) sendSync(pin rpio.Pin, pulseLength int) {
+	t.transmit(pin, 1, 31, pulseLength)
+}
+
+func (t *NativeTransmitter) transmit(pin rpio.Pin, highPulses int, lowPulses int, pulseLength int) {
+	pin.High()
+	time.Sleep(time.Microsecond * time.Duration(pulseLength*highPulses))
+	pin.Low()
+	time.Sleep(time.Microsecond * time.Duration(pulseLength*lowPulses))
 }
