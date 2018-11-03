5ce7c9cb70963e2b578da605ac3003f84d4570f6
Martin Ohmann <martin.ohmann@lesara.de>
2018-11-03 13:30:36 +0100

    [gpio] replace gpio library

--- internal/gpio/transmitter.go
+++ internal/gpio/transmitter.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/brian-armstrong/gpio"
-	rpio "github.com/stianeikeland/go-rpio"
 )
 
 const (
@@ -51,20 +50,14 @@ type CodeTransmitter interface {
 
 // NativeTransmitter type definition
 type NativeTransmitter struct {
-	gpioPin  rpio.Pin
+	gpioPin  gpio.Pin
 	protocol protocol
 }
 
 func NewNativeTransmitter(gpioPin int) (*NativeTransmitter, error) {
-	if err := rpio.Open(); err != nil {
-		return nil, err
-	}
-
 	t := &NativeTransmitter{
-		gpioPin: rpio.Pin(gpioPin),
+		gpioPin: gpio.NewOutput(uint(gpioPin), false),
 	}
-
-	t.gpioPin.Output()
 
 	return t, nil
 }
@@ -95,7 +88,9 @@ func (t *NativeTransmitter) Transmit(code uint64, protocol int, pulseLength int)
 
 // Close triggers rpio cleanup
 func (t *NativeTransmitter) Close() error {
-	return rpio.Close()
+	t.gpioPin.Close()
+
+	return nil
 }
 
 func (t *NativeTransmitter) selectProtocol(protocol int) error {
@@ -155,70 +150,5 @@ func NewTransmitter(gpioPin int) (CodeTransmitter, error) {
 		return NewNullTransmitter(gpioPin)
 	}
 
-	return NewSysFsTransmitter(gpioPin)
-}
-
-// SysFsTransmitter type definition
-type SysFsTransmitter struct {
-	gpioPin  gpio.Pin
-	protocol protocol
-}
-
-func NewSysFsTransmitter(gpioPin int) (*SysFsTransmitter, error) {
-	t := &SysFsTransmitter{
-		gpioPin: gpio.NewOutput(uint(gpioPin), false),
-	}
-
-	return t, nil
-}
-
-// Transmit transmits a code using given protocol and pulse length
-func (t *SysFsTransmitter) Transmit(code uint64, protocol int, pulseLength int) error {
-	logger.Printf("transmitting code=%d pulseLength=%d\n", code, pulseLength)
-
-	if err := t.selectProtocol(protocol); err != nil {
-		return err
-	}
-
-	t.setPulseLength(pulseLength)
-
-	for retry := 0; retry < numRetries; retry++ {
-		for j := bitLength - 1; j >= 0; j-- {
-			if code&(1<<uint64(j)) > 0 {
-				t.transmit(t.protocol.one)
-			} else {
-				t.transmit(t.protocol.zero)
-			}
-		}
-		t.transmit(t.protocol.sync)
-	}
-
-	return nil
-}
-
-// Close triggers rpio cleanup
-func (t *SysFsTransmitter) Close() error {
-	return rpio.Close()
-}
-
-func (t *SysFsTransmitter) selectProtocol(protocol int) error {
-	if protocol < 1 || protocol > len(protocols) {
-		return fmt.Errorf("Protocol %d does not exist", protocol)
-	}
-
-	t.protocol = protocols[protocol-1]
-
-	return nil
-}
-
-func (t *SysFsTransmitter) setPulseLength(pulseLength int) {
-	t.protocol.pulseLength = pulseLength
-}
-
-// transmit sends a sequence of high and low pulses on the gpio pin
-func (t *SysFsTransmitter) transmit(pulses highLow) {
-	t.gpioPin.High()
-	time.Sleep(time.Microsecond * time.Duration(t.protocol.pulseLength*pulses.high))
-	t.gpioPin.Low()
-	time.Sleep(time.Microsecond * time.Duration(t.protocol.pulseLength*pulses.low))
+	return NewNativeTransmitter(gpioPin)
 }
