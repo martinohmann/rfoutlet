93c6a9fb58b67f9217d230856af5973bf3bbd28a
Martin Ohmann <martin.ohmann@lesara.de>
2018-11-07 11:07:00 +0100

    [master] simplify

--- pkg/gpio/protocol.go
+++ pkg/gpio/protocol.go
@@ -14,9 +14,9 @@ type Protocol struct {
 // Protocols defines known remote control protocols. These are exported to give
 // users the ability to add more protocols if needed.
 var Protocols = []Protocol{
-	Protocol{350, HighLow{1, 31}, HighLow{1, 3}, HighLow{3, 1}},
-	Protocol{650, HighLow{1, 10}, HighLow{1, 2}, HighLow{2, 1}},
-	Protocol{100, HighLow{30, 71}, HighLow{4, 11}, HighLow{9, 6}},
-	Protocol{380, HighLow{1, 6}, HighLow{1, 3}, HighLow{3, 1}},
-	Protocol{500, HighLow{6, 14}, HighLow{1, 2}, HighLow{2, 1}},
+	{350, HighLow{1, 31}, HighLow{1, 3}, HighLow{3, 1}},
+	{650, HighLow{1, 10}, HighLow{1, 2}, HighLow{2, 1}},
+	{100, HighLow{30, 71}, HighLow{4, 11}, HighLow{9, 6}},
+	{380, HighLow{1, 6}, HighLow{1, 3}, HighLow{3, 1}},
+	{500, HighLow{6, 14}, HighLow{1, 2}, HighLow{2, 1}},
 }
